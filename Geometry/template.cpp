b60aa2c24bed9c0aa5f4b5d27f6c4cf41f89889e
everysick <s.wakeup31@gmail.com>
2014-12-09 13:37:49 +0900

    Added cross check

--- Geometry/template.cpp
+++ Geometry/template.cpp
@@ -1,9 +1,27 @@
+//点と線
+typedef double POINT_TYPE
+typedef pair<POINT_TYPE, POINT_TYPE> point;
+typedef pair<point, point > line;
+
 //外積
-double cross(Point a, Point b) {
+POINT_TYPE cross(Point a, Point b) {
   return (a.fi * b.se - a.se * b.fi);
 }
 
 //内積
-double dot(Point a, Point b) {
+POINT_TYPE dot(Point a, Point b) {
   return (a.fi * b.fi + a.se * b.se);
 }
+
+//線分の差
+point dist(point a, point b)
+{
+  return point(a.fi - b.fi, a.se - b.se);
+}
+
+//線分の交差判定
+bool is_cross(line a, line b)
+{
+  return (((cross(dist(a.se, a.fi), dist(b.fi, a.fi))) * (cross(dist(a.se, a.fi), dist(b.se, a.fi)))) <= 0) && (((cross(dist(b.se, b.fi), dist(a.fi, b.fi))) * (cross(dist(b.se, b.fi), dist(a.se, b.fi)))) <= 0);
+}
+
