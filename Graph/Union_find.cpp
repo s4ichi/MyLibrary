2fdc5332e45fe9717f5638cd459e3a43c29d6dc5
everysick <s.wakeup31@gmail.com>
2016-06-23 23:42:01 +0900

    Update union find

--- Graph/Union_find.cpp
+++ Graph/Union_find.cpp
@@ -1,17 +1,26 @@
 struct UF {
-  vector<int> p;
-  UF(){} UF(int n):p(n,-1){}
+	vector<int> p;
+	UF(int n):p(n,-1){}
 
-  int find(int x){return p[x]<0 ? x : p[x]=find(p[x]);}
-  int same(int a, int b){return find(a)==find(b);}
-  int size(int x){return p.size();}
+	int find(int x){
+		return p[x]<0 ? x : p[x] = find(p[x]);
+	}
 
-  void unit(int x,int y)
-  {
-    x=find(x);y=find(y);
-    if(x==y)return;
-    if(p[x]<p[y])swap(x,y);
-    p[y]+=p[x];
-    p[x]=y;
-  }
+	int same(int a, int b){
+		return find(a) == find(b);
+	}
+
+	int size(int x){
+		return -p[find(x)];
+	}
+
+	bool unite(int x, int y)
+	{
+		x = find(x); y = find(y);
+		if(x == y) return false;
+		if(p[y] < p[x]) swap(x,y);
+		p[x] += p[y];
+		p[y] = x;
+		return true;
+	}
 };
