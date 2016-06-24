61e492ecb82f51e338f74c07485891d2be43544b
everysick <s.wakeup31@gmail.com>
2016-06-25 01:01:33 +0900

    Add convert script

---
+++ convert_ps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if type "enscript" > /dev/null 2>&1; then
+    :
+else
+    echo "Not found command: enscript"
+    exit 1
+fi
+
+if type "pstopdf" > /dev/null 2>&1; then
+    :
+else
+    echo "Not found command: pstopdf"
+    exit 1
+fi
+
+touch tmp.cpp
+echo "// My library \n // contest template" >> tmp.cpp
+cat contest_template.cpp >> tmp.cpp
+
+for dir in *; do
+    if [ -d ${dir} ]; then
+        touch tmp_${dir}.cpp
+
+        cd ${dir}
+        for i in *.cpp; do
+            echo "// ${dir}\n" >> ../tmp_${dir}.cpp
+            cat ${i} >> ../tmp_${dir}.cpp
+            echo "\n\n" >> ../tmp_${dir}.cpp
+        done
+        cd ..
+    fi
+done
+
+enscript --highlight=cpp --fancy-header --line-number --color --font=Courier5 --landscape --columns=2 --indent=2 --tabsize=2 -o output.ps ./tmp*.cpp
+rm ./tmp*.cpp
+rm library.pdf
+pstopdf -o library.pdf output.ps
+rm output.ps
